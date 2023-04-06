31b54efaccbbfed857d876b154cc5f29348bd66e
agusrnfr <agustina.rojas193292@alumnos.info.unlp.edu.ar>
2023-04-06 12:48:13 -0300

    sigue mal el nombre

--- PRACTICAS/Ejercicio3-MediaPlayer/src/main/java/ar/edu/unlp/info/oo1/Ejercicio3_MediaPlayer/Media.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlp.info.oo1.ejercicio3_MediaPlayer;
-
-public abstract class Media {
-	
-	public abstract String play();
-
-}
--- PRACTICAS/Ejercicio3-MediaPlayer/src/main/java/ar/edu/unlp/info/oo1/Ejercicio3_MediaPlayer/VideoStream.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlp.info.oo1.ejercicio3_MediaPlayer;
-
-public class VideoStream {
-	
-	public String reproduce() {
-		return "Lana Del Rey - Summertime Sadness (Official Music Video)";
-	}
-
-}
